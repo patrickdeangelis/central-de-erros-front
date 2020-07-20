6ab96d70acb718eef5f60e77624b03cccefba7f7
patrick <patrick.angelis@hotmail.com>
2020-07-20 20:34:59 -0300

    Update route with new pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,12 +5,18 @@ import PrivateRoute from './PrivateRoute';
 import SignIn from "../pages/SignIn";
 import Dashboard from '../pages/Dashboard'
 import Details from '../pages/Details';
+import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword"
+import ResetPassword from "../pages/ResetPassword"
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={SignIn} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/forgotpassword" exact component={ForgotPassword} />
+        <Route path="/resetpassword/:uid/:token/" exact component={ResetPassword} />
         <PrivateRoute path="/dashboard" exact component={Dashboard} />
         <PrivateRoute path="/details/:id" exact component={Details} />
       </Switch>
